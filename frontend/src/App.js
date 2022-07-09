b03055107db8fa86276113bb63845f5bdb1eb944
Joe Stanley <engineerjoe440@yahoo.com>
2022-07-08 20:49:29 -0700

    basic knob

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import Knob from './knob';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Knob
+          size={50}
+          numTicks={7}
+          degrees={260}
+          min={1}
+          max={100}
+          value={30}
+        />
     </div>
   );
 }
