6cfafd254fcc4be5733178c7de8c1be5679a4a76
Joe Stanley <engineerjoe440@yahoo.com>
2022-07-08 20:18:25 -0700

    initial frontend

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,40 @@ VMeeterWeb - A Web Interface for VMeeter Applications.
 """
 ################################################################################
 
-from fastapi import Depends, FastAPI
-import voicemeeterlib as vmeeterlib
+from uuid import uuid4
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+#import voicemeeterlib as vmeeterlib
+
+# Application Base
+app = FastAPI()
+
+TEMPLATES = None
+
+@app.on_event("startup")
+async def startup_event():
+    """Event that Only Runs When App is Starting"""
+    global TEMPLATES
+    # Mount the Static File Path
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+    TEMPLATES = Jinja2Templates(directory="templates")
+
+# Main Application Response
+@app.get("/", response_class=HTMLResponse)
+@app.get('/index', response_class=HTMLResponse)
+@app.get('/index.html', response_class=HTMLResponse)
+@app.get('/index.htm', response_class=HTMLResponse)
+async def root(request: Request):
+    return TEMPLATES.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "fastapi_token": uuid4(),
+        },
+    )
+
 
 
 
